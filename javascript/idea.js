afb810dc4e701d8ab3c868d542655a34c687ee04
Sam Lucas <72397809+samlucas055@users.noreply.github.com>
2023-05-02 23:19:49 -0400

    added spotify api call

--- javascript/idea.js
+++ javascript/idea.js
@@ -10,15 +10,20 @@ searchForm.onsubmit = (ev) => {
         let button = document.createElement("button");
         button.innerText = data[0][0][0];
         resultsUL.appendChild(button);
-        
+        button.onclick = (e) => {
+            //const token = getToken();
+            searchForSong(data[0][0][0])
+            .then(response => response.json())
+            .then(track => track.tracks.items) // get the list of songs
+            .then(songs => {
+                console.log(songs);
+                songs.forEach((song) => {
+                    console.log(song.name);
+                });
+            });
+        }
 
     })
-
-
-
-
-
-
 };
 
 // const translatePromise = translate(queryText);
@@ -40,15 +45,33 @@ function translate(word) {
     // return data;
     return translatePromise
 
-
-
-
 };
 
-
-
-
-
+function getToken () {
+    return fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa('e59ac2382d5a4b4982c25560396ff044' + ':' + '179e3eceddeb438d92f371bb4387aac1')
+      }, // don't know why btoa is needed, but it doesn't work without it
+      body: 'grant_type=client_credentials'
+    })
+    .then(response => response.json())
+    .then(data => data.access_token);
+  };
+  
+  function searchForSong(songTitle) {
+    return getToken() // get the access token
+        .then (token => {
+            // use the access token and search for songs with titles that match the songTitle
+            return fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(songTitle)}&type=track`, {
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                }
+            })
+        })
+  };
+  
 
 
 
